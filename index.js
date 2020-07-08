f2a3594bcf2f6c7649a79e4cf68b4f3f753c95e6
Keith Isham <kisham8899@gmail.com>
2020-07-08 21:19:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function moveDodgerRight() {
     dodger.style.right = `${right + 1}px`;
   }
 }
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
