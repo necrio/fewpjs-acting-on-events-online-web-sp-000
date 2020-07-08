fd623eb59e57fdc0a91d9db9fe18f7487c989b29
Keith Isham <kisham8899@gmail.com>
2020-07-08 21:18:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function moveDodgerRight() {
   let right = parseInt(leftNumbers, -10);
  
   if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
+    dodger.style.right = `${right + 1}px`;
   }
 }
